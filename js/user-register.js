bf545a659a7f41aa1f47d9f15c6b9997d29b8965
Mehmetkrks1 <mehmetkrks438@gmail.com>
2024-09-17 01:36:27 +0300

    update

---
+++ js/user-register.js
@@ -0,0 +1,11 @@
+const loginEmail = "mehmetkrks438@gmail.com";
+const loginPassword = "20Krksmehmet03";
+
+const emailInput = document.getElementById("loginEmail");
+const passwordInput = document.getElementById("loginPassword");
+
+const userName = emailInput.value;
+const userPassword = passwordInput.value;
+
+console.log(userName);
+console.log(userPassword);
